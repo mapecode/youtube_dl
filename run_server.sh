2b480626a927e004ab06d78691e21f80c7ab4005
Mario Pérez <mario.perez17@alu.uclm.es>
2019-11-21 01:25:29 +0100

    Corregido error proceso y espera al downloader

--- run_server.sh
+++ run_server.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-python3 src/downloader.py --Ice.Config=server.config | tee /tmp/downloader-proxy.out &
+python3 src/downloader.py --Ice.Config=server.config > /tmp/downloader-proxy.out &
 PID=$!
 
-sleep .5
+sleep 2
 
 python3 src/orchestrator.py --Ice.Config=server.config "$(head -1 /tmp/downloader-proxy.out)" | tee /tmp/orchestrator-proxy.out
 
 kill -KILL $PID
-rm /tmp/*.out
