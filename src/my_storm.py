95f70e4847676b785147a673b92868995393fab3
Mario Pérez <mario.perez17@alu.uclm.es>
2019-12-05 23:54:15 +0100

    Create my_storm.py

---
+++ src/my_storm.py
@@ -0,0 +1,14 @@
+import IceStorm
+
+ORCHESTRATOR_TOPIC_NAME = 'OrchestratorSync'
+DOWNLOADER_TOPIC_NAME = 'UpdateSync'
+
+
+def get_topic_manager(broker, topic_sync):
+    key = 'IceStorm.TopicManager.Proxy'
+    topic_manager_proxy = broker.propertyToProxy(key)
+
+    if topic_manager_proxy is None:
+        raise ValueError("property {} not set".format(key))
+
+    return IceStorm.TopicManagerPrx.checkedCast(topic_manager_proxy)
