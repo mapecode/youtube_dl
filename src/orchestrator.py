07342d1ecbd0e1ce2ae6eea122ec61f5b12e6ebd
Mario Pérez <mario.perez17@alu.uclm.es>
2019-12-06 20:18:07 +0100

    Version incompleta

--- src/orchestrator.py
+++ src/orchestrator.py
@@ -1,15 +1,26 @@
 #!/usr/bin/python3
-
+"""Dudas
+¿Como se manejan varios orchestrators?
+¿Cada vez que se crea un orchestrator se crea la estructura de datos que los contiene?
+¿El cliente como se conecta con un orchestrator si hay varios lanzados?
+¿Cual es la secuencia de pasos que se deben seguir al ejecutar un orchestrator?
+¿Los orchestrators son publicadores y suscriptores de ambos canales?
+¿Por que las clases de los eventos tienen que tener asociadas un orchestrator?
+¿Es correcta la asociacion del orchestrator a una clase de evento pasando la instancia del
+orchestrator al crear el evento?
+"""
 import sys
 import Ice
-from color import Color
+from src.color import Color
+import src.my_storm as my_storm
 
 Ice.loadSlice('trawlnet.ice')
 import TrawlNet
 
 
 class OrchestratorI(TrawlNet.Orchestrator):
-    def __init__(self, downloader):
+    def __init__(self, orchestrator, downloader):
+        self.orchestrator = orchestrator
         self.downloader = downloader
 
     def downloadTask(self, url, current=None):
@@ -17,20 +28,85 @@ class OrchestratorI(TrawlNet.Orchestrator):
             return self.downloader.addDownloadTask(url)
 
     def getFileList(self, current=None):
-        pass
-
-    def hello(self, me, current=None):
-        pass
+        raise NotImplementedError()
 
     def announce(self, other, current=None):
-        pass
+        raise NotImplementedError()
 
 
+class OrchestratorEventI(TrawlNet.OrchestratorEvent):
+    def __init__(self, orchestrator):
+        self.orchestrator = orchestrator
+
+    def hello(self, me, current=None):
+        raise NotImplementedError()
+
+
+class UpdateEventI(TrawlNet.UpdateEvent):
+    def __init__(self, orchestrator):
+        self.orchestrator = orchestrator
+
+    def newFile(self, file, current=None):
+        raise NotImplementedError()
+
+
+class Orchestrator:
+    def __init__(self, broker, downloader_prx):
+        self.orchestrators_dic = {}
+        self.files_dic = {}
+        self.adapter = broker.createObjectAdapter('OrchestratorAdapter')
+        self.downloader = TrawlNet.DownloaderPrx.checkedCast(broker.stringToProxy(downloader_prx))
+
+        if not self.downloader:
+            raise ValueError(Color.BOLD + Color.RED + 'Invalid proxy ' + Color.END)
+
+        # IceStorm connect with my_storm
+        self.topic = my_storm.get_topic_manager(broker)
+        if not self.topic:
+            raise ValueError(Color.BOLD + Color.RED + 'Invalid proxy in topic manager' + Color.END)
+
+        # Orchestrator subscriber event
+        self.subscriber = OrchestratorEventI(self)
+        self.subscriber_prx = self.adapter.addWithUUID(self.subscriber)  # proxy
+
+        try:
+            self.topic = self.topic.retrieve(my_storm.ORCHESTRATOR_TOPIC_NAME)
+        except my_storm.EXCEPTION:
+            self.topic = self.topic.create(my_storm.ORCHESTRATOR_TOPIC_NAME)
+
+        self.topic.subscribeAndGetPublisher({}, self.subscriber_prx)
+
+        # Orchestrator publisher event
+        self.publisher = TrawlNet.OrchestratorEventPrx.uncheckedCast(self.topic.getPublisher())
+
+        # Orchestrator servant
+        self.servant = OrchestratorI(self, self.downloader)
+        self.servant_prx = self.adapter.addWithUUID(self.servant)  # proxy
+
+    def send_download_task(self, url):
+        raise NotImplementedError()
+
+    def new_orchestrator(self, orchestrator):
+        raise NotImplementedError()
+
+    def hello_to(self, orchestrator):
+        raise NotImplementedError()
+
+    def get_files(self):
+        raise NotImplementedError()
+
+    def start_servant(self):
+        self.adapter.activate()
+
+    def stop(self):
+        self.topic.unsubscribe(self.subscriber_prx)
+
+
+"""Server de fase 1
 class Server(Ice.Application):
     def run(self, argv):
         if len(argv) < 2:
-            print(Color.BOLD + Color.RED + 'Error in arguments' + Color.END)
-            return -1
+            ValueError(Color.BOLD + Color.RED + 'Error in arguments' + Color.END)
 
         broker = self.communicator()
 
@@ -50,6 +126,13 @@ class Server(Ice.Application):
         broker.waitForShutdown()
 
         return 0
+"""
+
+
+class Server(Ice.Application):
+    def run(self, argv):
+        if len(argv) < 2:
+            ValueError(Color.BOLD + Color.RED + 'Error in arguments' + Color.END)
 
 
 if __name__ == '__main__':
