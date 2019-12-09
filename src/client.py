199dda0705dd5957bb0df483fd5a18543d9cfbfd
mapecode <mario.perez17@alu.uclm.es>
2019-12-09 21:22:06 +0100

    Error print corregido

--- src/client.py
+++ src/client.py
@@ -3,6 +3,7 @@ import sys
 import Ice
 from color import Color
 
+""" si se recibe url se descargar si no se imprime la lista de ficheros"""
 Ice.loadSlice('trawlnet.ice')
 import TrawlNet
 
@@ -16,7 +17,8 @@ class Client(Ice.Application):
         if not orchestrator:
             raise RuntimeError(Color.BOLD + Color.RED + 'Invalid proxy' + Color.END)
 
-        print(Color.GREEN+"\nmp3 downloaded => '"+Color.BOLD+orchestrator.downloadTask(argv[2])+"'\n"+Color.END)
+        print(Color.GREEN + "\nmp3 downloaded => '" + Color.BOLD + str(
+            orchestrator.downloadTask(argv[2])) + "'\n" + Color.END)
 
         return 0
 
