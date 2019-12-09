31e472237177331c072d6b67b1dc112ee16b2bcd
mapecode <mario.perez17@alu.uclm.es>
2019-12-09 19:15:38 +0100

    get topic implementado en my_storm

--- src/my_storm.py
+++ src/my_storm.py
@@ -1,8 +1,8 @@
 import IceStorm
+from src.color import Color
 
 ORCHESTRATOR_TOPIC_NAME = 'OrchestratorSync'
 DOWNLOADER_TOPIC_NAME = 'UpdateEvents'
-EXCEPTION = IceStorm.NoSuchTopic
 
 
 def get_topic_manager(broker):
@@ -12,4 +12,16 @@ def get_topic_manager(broker):
     if topic_manager_proxy is None:
         raise ValueError("property {} not set".format(key))
 
-    return IceStorm.TopicManagerPrx.checkedCast(topic_manager_proxy)
+    topic_manager = IceStorm.TopicManagerPrx.checkedCast(topic_manager_proxy)
+
+    if not topic_manager:
+        raise ValueError(Color.BOLD + Color.RED + 'Invalid proxy in topic manager' + Color.END)
+
+    return topic_manager
+
+
+def get_topic(topic_manager, topic):
+    try:
+        return topic_manager.retrieve(topic)
+    except IceStorm.NoSuchTopic:
+        return topic_manager.create(topic)
