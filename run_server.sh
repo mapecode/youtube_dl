7c8ca6199f2414286142e37ed6a1f2ad20a97ff2
Mario Pérez <mario.perez17@alu.uclm.es>
2020-01-18 15:25:07 +0100

    Update run_server.sh
    Se ha corregido algunos directorios que daban error en icegridgui

--- run_server.sh
+++ run_server.sh
@@ -15,12 +15,18 @@ icegridnode --Ice.Config=registry-node.config &
 sleep 2
 
 echo "Exec downloads-node"
-mkdir -p /tmp/db/downloads-node/servers
+mkdir -p /tmp/db/downloads-node/servers/distrib/YoutubeDownloaderApp
+cp trawlnet.ice ./src/downloader_factory.py ./src/transfer_factory.py ./src/utils.py \
+/tmp/db/downloads-node/servers/distrib/YoutubeDownloaderApp
+chmod +x /tmp/db/downloads-node/servers/distrib/YoutubeDownloaderApp/*
 icegridnode --Ice.Config=downloads-node.config &
 sleep 2
 
 echo "Exec orchestrator-node"
-mkdir -p /tmp/db/orchestrator-node/servers
+mkdir -p /tmp/db/orchestrator-node/servers/distrib/YoutubeDownloaderApp
+cp trawlnet.ice ./src/orchestrator.py ./src/utils.py \
+/tmp/db/orchestrator-node/servers/distrib/YoutubeDownloaderApp
+chmod +x /tmp/db/orchestrator-node/servers/distrib/YoutubeDownloaderApp/*
 icegridnode --Ice.Config=orchestrator-node.config
 
 echo "Shoutting down..."
